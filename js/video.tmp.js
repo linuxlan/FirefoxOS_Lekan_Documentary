06bded8091933d03323c7ff2f24305b26b29444c
Eric.ye <wenlong.ye@mail.lekan.com>
2013-07-22 14:40:06 +0800

    初始化

---
+++ js/video.tmp.js
@@ -0,0 +1,12 @@
+--2013-07-10 11:27:24--  http://vjs.zencdn.net/c/video.js
+Resolving vjs.zencdn.net... 23.46.133.38
+Connecting to vjs.zencdn.net|23.46.133.38|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [application/javascript]
+Saving to: ‘video.js.2’
+
+     0K .......... .......... .......... .......... ..........  183K
+    50K .......... ..                                          98.3K=0.4s
+
+2013-07-10 11:27:25 (156 KB/s) - ‘video.js.2’ saved [63939]
+
